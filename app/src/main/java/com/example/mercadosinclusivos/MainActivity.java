4da5fdc509e15bce91fb33dbefdd5d138785fc12
rodritm <rodritm2010@gmail.com>
2019-06-10 14:31:32 -0400

    Creación de todos los primeros formularios de los productos

--- app/src/main/java/com/example/mercadosinclusivos/MainActivity.java
+++ app/src/main/java/com/example/mercadosinclusivos/MainActivity.java
@@ -2,12 +2,19 @@ package com.example.mercadosinclusivos;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    //    android:id="@+id/ini_botonIngresar"
+    //    android:id="@+id/ini_botonRegistrarse"
+    Button Ingresar;
+    Button Registrarse;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.content_formulario_papa1);
+        setContentView(R.layout.content_registro);
     }
+
+
 }
