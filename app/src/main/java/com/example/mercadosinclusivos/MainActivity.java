2450df60dd912415de974116ab88469e320367f8
rodritm <rodritm2010@gmail.com>
2019-06-11 10:09:32 -0400

    Agregando formulario2 e interacción entre vistas

--- app/src/main/java/com/example/mercadosinclusivos/MainActivity.java
+++ app/src/main/java/com/example/mercadosinclusivos/MainActivity.java
@@ -1,20 +1,42 @@
 package com.example.mercadosinclusivos;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
-    //    android:id="@+id/ini_botonIngresar"
-    //    android:id="@+id/ini_botonRegistrarse"
-    Button Ingresar;
-    Button Registrarse;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.content_registro);
+        setContentView(R.layout.activity_main);
+
+        Button registrarse = (Button) findViewById(R.id.ini_botonRegistrarse);
+        registrarse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, Registro.class);
+                startActivity(i);
+
+            }
+        });
+
+        Button iniciar= (Button) findViewById(R.id.ini_botonIngresar);
+        iniciar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, Pantalla_Inicial.class);
+                startActivity(i);
+
+            }
+        });
+
+
+
     }
-
-
 }
